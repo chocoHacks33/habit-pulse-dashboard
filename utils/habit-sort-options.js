4893da30c5298f2a19b319ed506cba4f68dd8d77
Prakash <45714711+chocoHacks33@users.noreply.github.com>
2026-08-13 09:09:01 +0800

    Add habit sort options

---
+++ utils/habit-sort-options.js
@@ -0,0 +1,5 @@
+export const habitSortOptions = [
+  { value: 'progress-desc', label: 'Progress: high to low' },
+  { value: 'progress-asc', label: 'Progress: low to high' },
+  { value: 'name-asc', label: 'Name: A to Z' },
+];
